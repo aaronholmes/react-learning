48eef7461cb21c44414f4aa211b6bf06f73c2092
Aaron Holmes <aholmes@pureguru-software.com>
2017-09-14 11:07:26 -0400

    Adding state and upvote

--- voting/public/js/app.js
+++ voting/public/js/app.js
@@ -1,4 +1,9 @@
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+
+    console.log(productId);
+
+  }
   render() {
     const sortedProducts = Seed.products.sort((a,b) => ( b.votes - a.votes));
     const productComponents = sortedProducts.map((product) => (
@@ -11,6 +16,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />));
 
     return (
@@ -22,6 +28,14 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
   render() {
     return (
       <div className='item'>
@@ -30,7 +44,7 @@ class Product extends React.Component {
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className='large caret up icon' />
             </a>
             {this.props.votes}
